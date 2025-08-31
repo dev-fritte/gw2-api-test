23c991049c59e09676b2dce09030860f71c33eb7
Patrick Schmidt <p.schmidt@goconut.com>
2025-08-31 10:42:29 +0200

    added react-hook-form

--- src/routes/account.tsx
+++ src/routes/account.tsx
@@ -2,32 +2,40 @@ import { createFileRoute } from '@tanstack/react-router'
 import { Input } from '@/components/ui/input.tsx'
 import useLocalStorage from '@/hooks/useLocalStorage.ts'
 import { Button } from '@/components/ui/button.tsx'
+import { Form, useForm } from 'react-hook-form'
 
 export const Route = createFileRoute('/account')({
     component: RouteComponent,
 })
 
+type FormData = {
+    apiKey: string;
+}
+
 function RouteComponent() {
 
     const [apiKey, setApiKey] = useLocalStorage<string | null>('apiKey', null)
-//TODO: add react hook form
+    const form = useForm<FormData>({
+        defaultValues: {
+            apiKey: apiKey ?? '',
+        }
+    })
 
-    const saveApiKey = (event) => () => {
-        console.log('test')
-        console.log('save api key', event.element.value);
-        setApiKey(event.element.value);
+    const saveApiKey = (data: FormData) => {
+        setApiKey(data.apiKey)
     }
 
     return <div className={'flex flex-col gap-10 p-10'}>
         <h1>Account</h1>
         <div className={'flex flex-col gap-10'}>
-            <Input className={'w-80'} placeholder={'API Key'} value={apiKey ?? undefined}/>
-            <Button onClick={(e) => {
-                console.log('save api key', e);
-                saveApiKey(e)
-            }}>
-                Save
-            </Button>
+            <Form control={form.control}
+                  onSubmit={({data}) => saveApiKey(data)}
+                  className={'flex flex-col gap-4'}>
+                <Input className={'w-80'} placeholder={'API Key'} {...form.register('apiKey')}/>
+                <Button type={'submit'}>
+                    Save
+                </Button>
+            </Form>
         </div>
     </div>
 }
