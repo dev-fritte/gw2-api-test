48c9319af5a993e700077ac3073cc8afb9d80121
Patrick Schmidt <p.schmidt@goconut.com>
2025-09-24 00:08:03 +0200

    added professions color to CharacterCard.tsx

--- src/character/components/CharacterCard.tsx
+++ src/character/components/CharacterCard.tsx
@@ -1,6 +1,7 @@
-import { type Character, ProfessionEnum } from '@/character/character-types.ts'
-import { useProfessions } from '@/profession/profession-queries.ts'
-import { getProfessionColor } from '@/profession/profession-utils.ts'
+import {type Character, ProfessionEnum} from '@/character/character-types.ts'
+import {useProfessions} from '@/profession/profession-queries.ts'
+import {getProfessionColor} from '@/profession/profession-utils.ts'
+import dayjs from 'dayjs'
 
 type CharacterCardProps = {
     character: Character;
@@ -17,24 +18,33 @@ export const CharacterCard = ({character}: CharacterCardProps) => {
     }
 
     const characterAge = (character?.age / 3600).toFixed(1)
-    console.log('profession', character?.profession, getProfessionColor(character.profession))
+    // console.log('profession', character, getProfessionColor(character.profession))
 
     return (
-        <div className={`w-72 h-80 flex flex-col gap-2 p-4 shadow-xl bg-[${getProfessionColor(character?.profession)}] rounded-md`}>
-            <div className={'flex justify-between'}>
-                <h3>{character.name}</h3>
-                <div className={'flex flex-col justify-center items-center'}>
-                    <img src={getProfessionIcon(character.profession)}/>
-                    <p>{character.level}</p>
+        <div className={`w-72 h-80 flex flex-col justify-between gap-2 p-4 shadow-xl rounded-md`}
+             style={{backgroundColor: `${getProfessionColor(character?.profession)}44`}}>
+
+            <div>
+                <div className={'flex justify-between'}>
+                    <h3>{character.name}</h3>
+                    <div className={'flex flex-col justify-center items-center'}>
+                        <img src={getProfessionIcon(character.profession)} alt={'profession icon'}/>
+                        <p>{character.level}</p>
+                    </div>
+                </div>
+                <div className={'flex gap-2'}>
+                    <p>Age:</p>
+                    <p>{`${characterAge} Hours`}</p>
+                </div>
+                <div className={'flex gap-2'}>
+                    <p>Deaths:</p>
+                    <p>{character.deaths}</p>
                 </div>
             </div>
+
             <div className={'flex gap-2'}>
-                <p>Age:</p>
-                <p>{`${characterAge} Hours`}</p>
-            </div>
-            <div className={'flex gap-2'}>
-                <p>Deaths:</p>
-                <p>{character.deaths}</p>
+                <p>Created:</p>
+                <p>{dayjs(character.created).format('MMMM YYYY')}</p>
             </div>
         </div>
     )
