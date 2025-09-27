f3489917b61aa3f00ac3656b217f9bcf5d2d1410
Patrick Schmidt <p.schmidt@goconut.com>
2025-09-27 02:48:00 +0200

    added query to load single character

--- src/character/character-queries.ts
+++ src/character/character-queries.ts
@@ -1,7 +1,7 @@
-import { useQuery } from '@tanstack/react-query'
-import { BASE_URL } from '@/utils/api-utils.ts'
-import { useApiKey } from '@/hooks/useApiKey.ts'
-import type { Character } from '@/character/character-types.ts'
+import {useQuery} from '@tanstack/react-query'
+import {BASE_URL} from '@/utils/api-utils.ts'
+import {useApiKey} from '@/hooks/useApiKey.ts'
+import type {Character} from '@/character/character-types.ts'
 
 export const useCharacters = () => {
 
@@ -18,3 +18,20 @@ export const useCharacters = () => {
         gcTime: 1000 * 60 * 60
     })
 }
+
+export const useCharacter = (name: string) => {
+
+    const apiKey = useApiKey()
+
+    return useQuery({
+        enabled: apiKey !== null,
+        queryKey: ['character', name],
+        queryFn: async () => {
+            return await fetch(`${BASE_URL}/characters/${name}?page=0&access_token=${apiKey}`)
+                .then(res => res.json())
+                .then(res => res as Character[])
+        },
+        gcTime: 1000 * 60 * 60
+    })
+}
+
