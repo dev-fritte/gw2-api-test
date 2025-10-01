b13d909a7146640b8f5f6422b93e5abe6454c274
Patrick Schmidt <p.schmidt@goconut.com>
2025-10-01 15:08:50 +0200

    added some new types (WeaponType, ItemRarity, DamageType)

--- src/item/types.ts
+++ src/item/types.ts
@@ -27,6 +27,47 @@ export enum ItemType {
     ARMOR = 'Armor',
 }
 
+export enum WeaponType {
+    AXE = 'Axe',
+    DAGGER = 'Dagger',
+    MACE = 'Mace',
+    PISTOL = 'Pistol',
+    SCEPTER = 'Scepter',
+    SWORD = 'Sword',
+    FOCUS = 'Focus',
+    SHIELD = 'Shield',
+    TORCH = 'Torch',
+    WARHORN = 'Warhorn',
+    GREATSWORD = 'Greatsword',
+    HAMMER = 'Hammer',
+    LONGBOW = 'LongBow',
+    RIFLE = 'Rifle',
+    SHORTBOW = 'ShortBow',
+    STAFF = 'Staff',
+    HARPOON = 'Harpoon',
+    SPEARGUN = 'Speargun',
+    TRIDENT = 'Trident',
+    LARGE_BUNDLE = 'LargeBundle',
+    SMALL_BUNDLE = 'SmallBundle',
+    TOY = 'Toy',
+    TOY_TWO_HANDED = 'ToyTwoHanded',
+}
+
 export enum ItemRarity {
+    JUNK = 'Junk',
+    BASIC = 'Basic',
+    FINE = 'Fine',
+    MASTERWORK = 'Masterwork',
+    RARE = 'Rare',
+    EXOTIC = 'Exotic',
+    ASCENDED = 'Assended',
     LEGENDARY = 'Legendary',
+}
+
+export enum DamageType {
+    FIRE = 'Fire',
+    ICE = 'Ice',
+    LIGHTNING = 'Lightning',
+    PHYSICAL = 'Physical',
+    CHOKING = 'Choking',
 }
